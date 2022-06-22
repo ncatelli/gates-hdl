0f5f913f865eb621530c4aee410ee3fb124f13ef
Nate Catelli <ncatelli@packetfire.org>
2022-06-22 08:10:30 -0500

    Implement parser (#3)
    * update grammar
    
    * Stub out ast
    
    * grammar update
    
    * Add identifier prefix
    
    * Implement first pass at parser
    
    * Add additional parser tests

---
+++ src/parser/ast.rs
@@ -0,0 +1,126 @@
+#[derive(Debug, PartialEq)]
+pub struct Definition(pub Vec<Directive>);
+
+#[derive(Debug, PartialEq)]
+pub struct Directive(pub DirectiveItem);
+
+#[derive(Debug, PartialEq)]
+pub enum DirectiveItem {
+    GateDef(GateDef),
+    LinkDef(LinkDef),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct GateDef {
+    identifier: GateIdentifier,
+    ty: GateTy,
+}
+
+impl GateDef {
+    pub fn new(identifier: GateIdentifier, ty: GateTy) -> Self {
+        Self { identifier, ty }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum GateTy {
+    Not,
+    And,
+    Or,
+    Xor,
+    Nand,
+    Nor,
+}
+
+impl From<GateTy> for &str {
+    fn from(gt: GateTy) -> Self {
+        match gt {
+            GateTy::Not => "not",
+            GateTy::And => "and",
+            GateTy::Or => "or",
+            GateTy::Xor => "xor",
+            GateTy::Nand => "nand",
+            GateTy::Nor => "nor",
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct GateIdentifier(String);
+
+impl GateIdentifier {
+    pub fn try_new(s: String) -> Option<Self> {
+        let head_is_valid = s
+            .chars()
+            .next()
+            .map(|c| c.is_ascii_lowercase())
+            .unwrap_or(false);
+        let tail_is_valid = s.chars().skip(1).all(|c| c.is_ascii_lowercase());
+
+        if head_is_valid && tail_is_valid {
+            Some(GateIdentifier(s))
+        } else {
+            None
+        }
+    }
+
+    pub fn try_new_unchecked(s: String) -> Self {
+        Self(s)
+    }
+}
+
+impl ToString for GateIdentifier {
+    fn to_string(&self) -> String {
+        self.as_ref().to_string()
+    }
+}
+
+impl AsRef<str> for GateIdentifier {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+impl From<GateIdentifier> for String {
+    fn from(gi: GateIdentifier) -> Self {
+        gi.to_string()
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct LinkDef {
+    src: GateIdentifier,
+    dest: GateIdentifier,
+    input: InputIdentifier,
+}
+
+impl LinkDef {
+    pub fn new(src: GateIdentifier, dest: GateIdentifier, input: InputIdentifier) -> Self {
+        Self { src, dest, input }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct InputIdentifier(pub char);
+
+impl InputIdentifier {
+    pub fn try_new(c: char) -> Option<Self> {
+        c.is_ascii_lowercase().then(|| Self(c))
+    }
+
+    pub fn try_new_unchecked(c: char) -> Self {
+        Self(c)
+    }
+
+    pub fn as_char(&self) -> char {
+        self.0
+    }
+}
+
+impl From<InputIdentifier> for char {
+    fn from(ii: InputIdentifier) -> Self {
+        ii.as_char()
+    }
+}
---
+++ src/parser/mod.rs
@@ -0,0 +1,203 @@
+use parcel::parsers::character::{any_character, expect_character, expect_str};
+use parcel::prelude::v1::*;
+
+mod ast;
+
+#[derive(PartialEq)]
+#[allow(unused)]
+pub enum ParseErr {
+    InvalidRule,
+    Undefined(String),
+}
+
+impl std::fmt::Debug for ParseErr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Undefined(err) => write!(f, "undefined parse error: {}", err),
+            Self::InvalidRule => write!(f, "provided rule is invalid",),
+        }
+    }
+}
+
+pub fn parse<S: AsRef<str>>(input: S) -> Result<ast::Definition, ParseErr> {
+    let input: Vec<(usize, char)> = input.as_ref().chars().enumerate().collect();
+    let result = match definition().parse(&input) {
+        Ok(MatchStatus::Match { inner, .. }) => Ok(inner),
+        Ok(MatchStatus::NoMatch { .. }) => Err(ParseErr::InvalidRule),
+        Err(e) => Err(ParseErr::Undefined(format!("{:?}", e))),
+    };
+
+    result
+}
+
+fn definition<'a>() -> impl Parser<'a, &'a [(usize, char)], ast::Definition> {
+    parcel::one_or_more(directive()).map(ast::Definition)
+}
+
+fn directive<'a>() -> impl Parser<'a, &'a [(usize, char)], ast::Directive> {
+    directive_item().map(ast::Directive)
+}
+
+fn directive_item<'a>() -> impl Parser<'a, &'a [(usize, char)], ast::DirectiveItem> {
+    parcel::or(gate_def().map(ast::DirectiveItem::GateDef), || {
+        link_def().map(ast::DirectiveItem::LinkDef)
+    })
+}
+
+fn gate_def<'a>() -> impl Parser<'a, &'a [(usize, char)], ast::GateDef> {
+    parcel::join(
+        parcel::right(parcel::join(
+            whitespace_wrapped(expect_str("DEFINE")),
+            whitespace_wrapped(gate_identifier()),
+        )),
+        parcel::right(parcel::join(
+            whitespace_wrapped(expect_str("AS")),
+            whitespace_wrapped(gate_ty()),
+        )),
+    )
+    .map(|(id, ty)| ast::GateDef::new(id, ty))
+}
+
+fn gate_ty<'a>() -> impl Parser<'a, &'a [(usize, char)], ast::GateTy> {
+    whitespace_wrapped(parcel::one_of(vec![
+        expect_str("not").map(|_| ast::GateTy::Not),
+        expect_str("and").map(|_| ast::GateTy::And),
+        expect_str("or").map(|_| ast::GateTy::Or),
+        expect_str("xor").map(|_| ast::GateTy::Xor),
+        expect_str("nand").map(|_| ast::GateTy::Nand),
+        expect_str("nor").map(|_| ast::GateTy::Nor),
+    ]))
+}
+
+fn link_def<'a>() -> impl Parser<'a, &'a [(usize, char)], ast::LinkDef> {
+    parcel::join(
+        parcel::right(parcel::join(
+            whitespace_wrapped(expect_str("LINK")),
+            whitespace_wrapped(gate_identifier()),
+        )),
+        parcel::join(
+            parcel::right(parcel::join(
+                whitespace_wrapped(expect_str("->")),
+                whitespace_wrapped(input_identifier()),
+            )),
+            parcel::right(parcel::join(
+                whitespace_wrapped(expect_str("OF")),
+                whitespace_wrapped(gate_identifier()),
+            )),
+        ),
+    )
+    .map(|(src, (input_id, dest))| ast::LinkDef::new(src, dest, input_id))
+}
+
+fn gate_identifier<'a>() -> impl Parser<'a, &'a [(usize, char)], ast::GateIdentifier> {
+    parcel::join(
+        any_character().predicate(|c| c.is_ascii_lowercase()),
+        parcel::one_or_more(parcel::or(
+            any_character().predicate(|c| c.is_ascii_lowercase()),
+            || expect_character('_'),
+        )),
+    )
+    .map(|(head, tail)| [head].iter().chain(tail.iter()).copied().collect())
+    // safe to call unchecked due to above guarantee
+    .map(ast::GateIdentifier::try_new_unchecked)
+}
+
+fn input_identifier<'a>() -> impl Parser<'a, &'a [(usize, char)], ast::InputIdentifier> {
+    any_character()
+        .predicate(|c| c.is_ascii_lowercase())
+        // safe to call unchecked due to above guarantee
+        .map(ast::InputIdentifier::try_new_unchecked)
+}
+
+fn whitespace_wrapped<'a, P, B>(parser: P) -> impl Parser<'a, &'a [(usize, char)], B>
+where
+    B: 'a,
+    P: Parser<'a, &'a [(usize, char)], B> + 'a,
+{
+    use parcel::parsers::character::whitespace;
+
+    parcel::right(parcel::join(
+        parcel::zero_or_more(whitespace()),
+        parcel::left(parcel::join(parser, parcel::zero_or_more(whitespace()))),
+    ))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::ast::*;
+    use super::*;
+
+    #[test]
+    fn should_parse_known_good_directive_varients() {
+        let inputs = vec![
+            // gate definition
+            (
+                "DEFINE gate AS and",
+                DirectiveItem::GateDef(GateDef::new(
+                    GateIdentifier::try_new_unchecked("gate".to_string()),
+                    GateTy::And,
+                )),
+            ),
+            // link definition
+            (
+                "LINK src -> a OF dest",
+                DirectiveItem::LinkDef(LinkDef::new(
+                    GateIdentifier::try_new_unchecked("src".to_string()),
+                    GateIdentifier::try_new_unchecked("dest".to_string()),
+                    InputIdentifier('a'),
+                )),
+            ),
+        ];
+
+        for (test_id, (input, expected_res)) in inputs.into_iter().enumerate() {
+            let parse_result = parse(&input);
+            assert_eq!(
+                (test_id, Ok(Definition(vec![Directive(expected_res)]))),
+                (test_id, parse_result)
+            )
+        }
+    }
+
+    #[test]
+    fn should_parse_multiline_definition() {
+        let (input, expected) = (
+            "DEFINE src AS and
+DEFINE dest AS not
+LINK src -> a OF dest",
+            vec![
+                Directive(DirectiveItem::GateDef(GateDef::new(
+                    GateIdentifier::try_new_unchecked("src".to_string()),
+                    GateTy::And,
+                ))),
+                Directive(DirectiveItem::GateDef(GateDef::new(
+                    GateIdentifier::try_new_unchecked("dest".to_string()),
+                    GateTy::Not,
+                ))),
+                Directive(DirectiveItem::LinkDef(LinkDef::new(
+                    GateIdentifier::try_new_unchecked("src".to_string()),
+                    GateIdentifier::try_new_unchecked("dest".to_string()),
+                    InputIdentifier('a'),
+                ))),
+            ],
+        );
+
+        let parse_result = parse(&input);
+        assert_eq!(Ok(Definition(expected)), parse_result)
+    }
+
+    #[test]
+    fn should_fail_to_parse_gate_definition_with_invalid_id() {
+        let input = "DEFINE InVaLiD AS and";
+
+        let parse_result = parse(&input);
+        assert!(parse_result.is_err())
+    }
+
+    #[test]
+    fn should_fail_to_parse_link_definition_with_invalid_input_id() {
+        let input = "LINK src -> 1 OF dest";
+
+        let parse_result = parse(&input);
+        assert!(parse_result.is_err())
+    }
+}
